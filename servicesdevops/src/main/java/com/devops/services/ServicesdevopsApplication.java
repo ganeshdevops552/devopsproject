a2a96c511e3078ca6849bea3915e9fb7725bd503
Mylearnings0430 <ingalenihar@gmail.com>
2023-04-22 17:15:05 +0530

    Spring boot project started

---
+++ servicesdevops/src/main/java/com/devops/services/ServicesdevopsApplication.java
@@ -0,0 +1,13 @@
+package com.devops.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesdevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesdevopsApplication.class, args);
+	}
+
+}
